1b4803526ee76f033c4619bf28fd6ee491335bc3
MargaritaSukhova <margarytasu@gmail.com>
2023-05-29 14:54:06 +0300

    search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { SearchForm } from './SearchBar.styled';
-// import {
-//   CloseButton,
-//   CloseIcon,
-//   FormInput,
-//   FormLabel,
-//   IconWrapper,
-//   SearchForm,
-//   SwapOffIcon,
-//   SwapOnIcon,
-// } from './SearchBar.styled';
+import {
+  CloseBtn,
+  CloseButton,
+  CloseIcon,
+  FormInput,
+  FormLabel,
+  IconWrapper,
+  SearchForm,
+  SearchIcon,
+  SwapOffIcon,
+  SwapOnIcon,
+} from './SearchBar.styled';
 
 const SearchBar = ({ onSubmit }) => {
   const [query, setQuery] = useState('');
@@ -32,11 +33,61 @@ const SearchBar = ({ onSubmit }) => {
   return (
     <div>
       <SearchForm onSubmit={handleSubmit}>
-        <label htmlFor="search">
-          <input value={query} onChange={handleChange} name="search" />
-        </label>
-        <button type="submit">Search</button>
+        <FormLabel htmlFor="search">
+          <FormInput
+            value={query}
+            onChange={handleChange}
+            name="search"
+            placeholder="search movies"
+            id="search"
+            type="text"
+          />
+          <SearchIcon>
+            <svg
+              stroke-width="2"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="swap-on"
+            >
+              <path
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                stroke-linejoin="round"
+                stroke-linecap="round"
+              ></path>
+            </svg>
+            <SwapOffIcon
+              stroke-width="2"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10 19l-7-7m0 0l7-7m-7 7h18"
+                stroke-linejoin="round"
+                stroke-linecap="round"
+              ></path>
+            </SwapOffIcon>
+          </SearchIcon>
+          <CloseBtn type="reset" class="close-btn">
+            <svg
+              viewBox="0 0 20 20"
+              class="h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                clip-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                fill-rule="evenodd"
+              ></path>
+            </svg>
+          </CloseBtn>
+          {/* <button type="submit">Search</button> */}
+        </FormLabel>
       </SearchForm>
+
       {/* <SearchForm>
         <FormLabel htmlFor="search">
           <FormInput
--- src/components/SearchBar/SearchBar.styled.js
+++ src/components/SearchBar/SearchBar.styled.js
@@ -2,6 +2,67 @@ import styled from '@emotion/styled';
 
 export const SearchForm = styled.form`
   margin-bottom: 30px;
+  width: 200px;
+  padding-inline-end: 1em;
+  background: #fff;
+  position: relative;
+  border-radius: 4px;
+  & svg {
+  display: block;
+  color: #111;
+  transition: 0.3s cubic-bezier(.4,0,.2,1);
+  position: absolute;
+  height: 15px;
+`;
+
+export const FormLabel = styled.label`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+`;
+
+export const FormInput = styled.input`
+  width: 100%;
+  padding-inline-start: calc(1.5em + 2em);
+  outline: none;
+  background: none;
+  border: 0;
+`;
+
+export const SearchIcon = styled.div`
+  position: absolute;
+  left: var1.5em;
+  transition: 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SwapOffIcon = styled.svg`
+ transform: rotate(-80deg);
+  opacity: 0;
+  visibility: hidden;
+}
+`;
+
+export const CloseBtn = styled.button`
+  background: none;
+  border: none;
+  right: calc(1.5em - 2em);
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #111;
+  padding: 0.1em;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  transition: 0.3s;
+  opacity: 0;
+  transform: scale(0);
+  visibility: hidden;
 `;
 
 // export const SearchForm = styled.form`
@@ -15,13 +76,6 @@ export const SearchForm = styled.form`
 //   background: #ffffff;
 //   position: relative;
 //   border-radius: 4px;
-// `;
-
-// export const FormLabel = styled.label`
-//   display: flex;
-//   align-items: center;
-//   width: 100%;
-//   height: 40px;
 // `;
 
 // export const IconWrapper = styled.div`
@@ -53,27 +107,6 @@ export const SearchForm = styled.form`
 //   transform: rotate(-80deg);
 //   opacity: 0;
 //   visibility: hidden;
-// `;
-
-// export const FormInput = styled.input`
-//   width: 100%;
-//   padding-inline-start: calc(1.5em + 2em);
-//   outline: none;
-//   background: none;
-//   border: 0;
-//   &:focus & ~ [IconWrapper] {
-//     transform: rotate(80deg) scale(1.3);
-//   }
-//   &: focus & ~[IconWrapper] [SwapOffIcon] {
-//     opacity: 1;
-//     transform: rotate(-80deg);
-//     visibility: visible;
-//     color: #15a986;
-//   }
-//   &: focus & ~[IconWrapper] [SwapOnIcon] {
-//     opacity: 0;
-//     visibility: visible;
-//   }
 // `;
 
 // export const CloseButton = styled.button`
